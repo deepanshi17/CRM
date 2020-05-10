0f180fbc1aa6f79f8646d1b7190d3a40ee875a3e
deepanshi17 <sharma.deepanshi@gmail.com>
2020-05-09 21:16:24 -0500

    init

---
+++ CMakeFiles/project7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project7.dir/Project7_starter_files/CustomerDB.cpp.o"
+  "CMakeFiles/project7.dir/Project7_starter_files/main.cpp.o"
+  "CMakeFiles/project7.dir/Project7_starter_files/Project7.cpp.o"
+  "project7.pdb"
+  "project7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
